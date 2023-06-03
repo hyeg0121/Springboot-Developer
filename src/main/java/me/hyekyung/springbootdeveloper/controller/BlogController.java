697bd3a8b0627dbe25b8dcbbecb4f77c398e728a
hyeg0121 <hyeg0121@naver.com>
2023-06-03 18:32:35 +0900

    [chap06] GET 컨트롤러 메서드 코드 작성

--- src/main/java/me/hyekyung/springbootdeveloper/controller/BlogController.java
+++ src/main/java/me/hyekyung/springbootdeveloper/controller/BlogController.java
@@ -3,13 +3,16 @@ package me.hyekyung.springbootdeveloper.controller;
 import lombok.RequiredArgsConstructor;
 import me.hyekyung.springbootdeveloper.domain.Article;
 import me.hyekyung.springbootdeveloper.dto.AddArticleRequest;
+import me.hyekyung.springbootdeveloper.dto.ArticleResponese;
 import me.hyekyung.springbootdeveloper.service.BlogService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController // HTTP 응답 본문에 객체 데이터를 JSON 형식으로 변환하는 컨트롤러
@@ -22,5 +25,14 @@ public class BlogController {
         Article savedArticle = blogService.save(request);
         return ResponseEntity.status(HttpStatus.CREATED)    // 201의 응답코드를 가지고
                 .body(savedArticle);    // 저장된 아티클의 정보
+    }
+
+    @GetMapping("/api/articles")
+    public ResponseEntity<List<ArticleResponese>> findAllArticles() {
+        List<ArticleResponese> articles = blogService.findAll()
+                .stream()
+                .map(ArticleResponese::new)
+                .toList();
+        return ResponseEntity.ok().body(articles);
     }
 }
--- src/main/java/me/hyekyung/springbootdeveloper/service/BlogService.java
+++ src/main/java/me/hyekyung/springbootdeveloper/service/BlogService.java
@@ -6,6 +6,8 @@ import me.hyekyung.springbootdeveloper.dto.AddArticleRequest;
 import me.hyekyung.springbootdeveloper.repository.BlogRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @RequiredArgsConstructor    // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
 @Service    // 빈으로 등록
 public class BlogService {
@@ -13,9 +15,9 @@ public class BlogService {
     private final BlogRepository blogRepository;
 
     /*
-    1. 클라이언트에서 글 생성 요청을 보낸다
-    2. AddArticleRequest라는 dto에 매핑이 된다
-    3. AddArticleRequest가 Article 엔티티로 변환이 도니다.
+    1. 클라이언트에서 글 생성 요청을 보냄
+    2. AddArticleRequest라는 dto에 매핑
+    3. AddArticleRequest가 Article 엔티티로 변환
     4. BlogRepository로 데이터 베이스에 저장
     5. 응답 요청
     */
@@ -24,5 +26,11 @@ public class BlogService {
     // 글 추가 메서드
     public Article save(AddArticleRequest request){
         return blogRepository.save(request.toEntity());
+    }
+
+    // 글을 모두 가져오는 메서드
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+        // JPA 지원 메소드인 findAll()을 호출해 article 테이블에 저장되어 있는 모든 데이터를 조회
     }
 }
